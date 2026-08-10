217933246a586f0a1387d674e683d337bdd6119a
morinjn <morinj@chelanschools.org>
2026-08-10 01:27:53 -0700

    Update config.js to 1.1.02 and ROOM 176

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /**
  * config.js - Global Configuration & Settings for Chelan High Systems
- * Version: 1.1.01
+ * Version: 1.1.02
  * 
  * Loaded globally via standard <script src="config.js?v=1.004"></script>
  */
@@ -8,9 +8,9 @@
 window.APP_CONFIG = {
     // Application Metadata
     appName: "Chelan High Medical Sciences Kiosk",
-    version: "1.1.01",
+    version: "1.1.02",
     schoolName: "Chelan High School",
-    department: "Room 176",
+    department: "ROOM 176",
 
     // Timeouts and Thresholds (In Minutes)
     settings: {
