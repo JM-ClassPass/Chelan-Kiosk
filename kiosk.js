bd188b953f916bf142d8624114a31978e7f3cd64
Jason Morin <morinj@chelanschools.org>
2026-08-27 14:05:38 -0700

    Update kiosk.js r10v2

--- kiosk.js
+++ kiosk.js
@@ -333,7 +333,7 @@ setPersistence(auth, browserSessionPersistence)
             studentId, name: fullName, type: 'Phone', details: 'COS', timestamp: serverTimestamp(), duration: durationStr
           });
 
-          showOverlay(`PHONE RETRIEVED`, `${studentData.firstName} removed phone from pocket ${oldPocket}.', 'Remember to put it in your backpack!`, 'success');
+          showOverlay(`PHONE RETRIEVED`, `${studentData.firstName} removed phone from pocket ${oldPocket}. PLEASE PUT IT IN YOUR BACKPACK!`, 'success');
           idInput.value = '';
         } else {
           // --- PHONE CHECK-IN LOGIC ---
