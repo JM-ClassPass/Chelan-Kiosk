cb6823ff159580ca0d2fa5eae4c6798ae87041c4
Jason Morin <morinj@chelanschools.org>
2026-08-27 14:01:34 -0700

    Update kiosk.js r10v1
    Backpack reminder for phone retrieval

--- kiosk.js
+++ kiosk.js
@@ -333,7 +333,7 @@ setPersistence(auth, browserSessionPersistence)
             studentId, name: fullName, type: 'Phone', details: 'COS', timestamp: serverTimestamp(), duration: durationStr
           });
 
-          showOverlay(`PHONE RETRIEVED`, `${studentData.firstName} removed phone from pocket ${oldPocket}`, 'success');
+          showOverlay(`PHONE RETRIEVED`, `${studentData.firstName} removed phone from pocket ${oldPocket}.', 'Remember to put it in your backpack!`, 'success');
           idInput.value = '';
         } else {
           // --- PHONE CHECK-IN LOGIC ---
